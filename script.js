b2c5f30ea06816342c2d8d3dffd0e3e1ec8ff5f3
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-25 10:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function volume_sphere() {
     //Write your code here
+	event.preventDefault();
+	let r = parseFloat(document.getElementById("radius").value);
+
+	let v = document.getElementById("volume");
+	if(isNaN(r) || r<0)
+	{
+		v.value = NaN;
+		return;
+	}
+	let volume = (4/3) * Math.PI * Math.pow(r, 3);
+		v.value = volume.toFixed(4); 
+
+	v.value = volume;
+	
   
 } 
 
